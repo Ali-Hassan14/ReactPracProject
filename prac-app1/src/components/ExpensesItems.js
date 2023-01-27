9550d09839eb2ed4272363ecf767d95c63ef6f81
AliHassan1407 <alihassan1407@outlook.com>
2023-01-27 19:10:44 +0500

    Made Code Clean

---
+++ prac-app1/src/components/ExpensesItems.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import ExpenseItem from './ExpenseItem'
+function ExpensesItems() {
+    const expenses = [
+    {
+      id:'e1',
+      title:'MBL LED Replacement',
+      amount:'R.s 8000',
+      date: new Date ()
+    },
+    {
+      id:'e2',
+      title:'Laptop Repair',
+      amount:'R.s 20000',
+      date: new Date ()
+    },
+    {
+      id:'e3',
+      title:'Water Cooler',
+      amount:'R.s 2000',
+      date: new Date ()
+    },
+    {
+      id:'e4',
+      title:'Buy Books',
+      amount:'R.s 5000',
+      date: new Date ()
+    }
+  ]
+  return (
+    <div> 
+    <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}></ExpenseItem>
+     <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}></ExpenseItem>
+     <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}></ExpenseItem>
+     <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date}></ExpenseItem>
+     </div>
+  )
+}
+
+export default ExpensesItems
