1447d394939405ecc9003a4a745f81224d13879f
AliHassan1407 <alihassan1407@outlook.com>
2023-01-30 23:57:33 +0500

    Clean code

--- prac-app1/src/components/ExpenseItem.js
+++ prac-app1/src/components/ExpenseItem.js
@@ -1,21 +1,24 @@
-import './ExpenseItem.css'
+import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
 function ExpenseItem (props) {
     //  const expenseDate= new Date();
     //  const expenseTitle='Mobile LED Replacement';
     //  const expenseAmount='Rs. 8000';
     return(
         <div className='expense-item'>
-            <div>{props.date.toDateString()}</div>
+            <div>
+                <ExpenseDate date={props.date}/>
+            </div>
             <div className='expense-item__description'>
                 <h2>{props.title}</h2>
             <div className='expense-item__price'>
                 <div>
                     {props.amount}
                 </div>
-            </div>
-                
+            </div>  
             </div>
         </div>
+        
     );
 }
 
--- prac-app1/src/components/ExpensesItems.js
+++ prac-app1/src/components/ExpensesItems.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ExpenseItem from './ExpenseItem'
+import './ExpensesItems.css'
 function ExpensesItems() {
     const expenses = [
     {
@@ -28,7 +29,7 @@ function ExpensesItems() {
     }
   ]
   return (
-    <div> 
+    <div className="expenses">
     <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}></ExpenseItem>
      <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}></ExpenseItem>
      <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}></ExpenseItem>
